691b63343e5e77756fb9c17ff203122ee4a52613
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-05-28 11:16:08 +0200

    chore: the java version of the project has been changed to 21 version v2.0

--- src/main/java/com/alfre/DHHotel/domain/event/ReservationCreatedEvent.java
+++ src/main/java/com/alfre/DHHotel/domain/event/ReservationCreatedEvent.java
@@ -2,6 +2,11 @@ package com.alfre.DHHotel.domain.event;
 
 import java.time.LocalDate;
 
-public record ReservationCreatedEvent(long reservationId, long clientId, long roomId, LocalDate startDate,
-                                      LocalDate endDate, String status) {
-}
+public record ReservationCreatedEvent(
+        long reservationId,
+        long clientId,
+        long roomId,
+        LocalDate startDate,
+        LocalDate endDate,
+        String status
+) { }
